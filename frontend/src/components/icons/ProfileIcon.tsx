b21d5cb8912e461c12d4c66a9a55d74c64eb2e23
kikilee-ato <kikilee.ato@gmail.com>
2026-04-09 11:57:23 +0900

    fix: 프로필 설정 - 사이드바 프로필 아이콘 디자인 변경

--- frontend/src/components/icons/ProfileIcon.tsx
+++ frontend/src/components/icons/ProfileIcon.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function ProfileIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <path d="M9.99967 8.33333C11.6105 8.33333 12.9163 7.0275 12.9163 5.41667C12.9163 3.80584 11.6105 2.5 9.99967 2.5C8.38884 2.5 7.08301 3.80584 7.08301 5.41667C7.08301 7.0275 8.38884 8.33333 9.99967 8.33333Z" stroke="currentColor" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round" />
-      <path d="M2.5 16.9998V17.4998H17.5V16.9998C17.5 15.133 17.5 14.1996 17.1367 13.4865C16.8171 12.8593 16.3072 12.3494 15.68 12.0298C14.9669 11.6665 14.0335 11.6665 12.1667 11.6665H7.83333C5.9665 11.6665 5.03308 11.6665 4.32004 12.0298C3.69283 12.3494 3.18289 12.8593 2.86331 13.4865C2.5 14.1996 2.5 15.133 2.5 16.9998Z" stroke="currentColor" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M9.99967 7.33333C11.6105 7.33333 12.9163 6.0275 12.9163 4.41667C12.9163 2.80584 11.6105 1.5 9.99967 1.5C8.38884 1.5 7.08301 2.80584 7.08301 4.41667C7.08301 6.0275 8.38884 7.33333 9.99967 7.33333Z" stroke="currentColor" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M2.5 15.9998V16.4998H17.5V15.9998C17.5 14.133 17.5 13.1996 17.1367 12.4865C16.8171 11.8593 16.3072 11.3494 15.68 11.0298C14.9669 10.6665 14.0335 10.6665 12.1667 10.6665H7.83333C5.9665 10.6665 5.03308 10.6665 4.32004 11.0298C3.69283 11.3494 3.18289 11.8593 2.86331 12.4865C2.5 13.1996 2.5 14.133 2.5 15.9998Z" stroke="currentColor" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
   );
 }
